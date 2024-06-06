60c497d422b1f27a89f8022ab09722246c66a576
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-06 19:30:51 +0530

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -10,54 +10,78 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	cout << unitbuf;
-	cerr << unitbuf;
+string extract_path(const string& request) {
+    size_t method_end = request.find(' ');
+    if (method_end == string::npos) return "";
 
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
-	{
-		cerr << "Failed to create server socket\n";
-		return 1;
-	}
+    size_t path_end = request.find(' ', method_end + 1);
+    if (path_end == string::npos) return "";
 
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-	{
-		cerr << "setsockopt failed\n";
-		return 1;
-	}
+    return request.substr(method_end + 1, path_end - method_end - 1);
+}
 
-	struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(4221);
+int main(int argc, char **argv) {
+    cout << unitbuf;
+    cerr << unitbuf;
 
-	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
-	{
-		cerr << "Failed to bind to port 4221\n";
-		return 1;
-	}
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        cerr << "Failed to create server socket\n";
+        return 1;
+    }
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0)
-	{
-		cerr << "listen failed\n";
-		return 1;
-	}
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        cerr << "setsockopt failed\n";
+        return 1;
+    }
 
-	struct sockaddr_in client_addr;
-	int client_addr_len = sizeof(client_addr);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
 
-	cout << "Waiting for a client to connect...\n";
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+        cerr << "Failed to bind to port 4221\n";
+        return 1;
+    }
 
-	int client = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-	string message = "HTTP/1.1 200 OK\r\n\r\n";
-	send(client, message.c_str(), message.length(), 0);
-	cout << "Client connected\n";
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        cerr << "listen failed\n";
+        return 1;
+    }
 
-	close(server_fd);
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
-	return 0;
+    cout << "Waiting for a client to connect...\n";
+
+    int client = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client < 0) {
+        cerr << "Failed to accept client connection\n";
+        close(server_fd);
+        return 1;
+    }
+
+    char buffer[1024] = {0};
+    read(client, buffer, sizeof(buffer) - 1);
+
+    string request(buffer);
+    string path = extract_path(request);
+
+    string response;
+    if (path == "/") {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
+    send(client, response.c_str(), response.length(), 0);
+    cout << "Client connected\n";
+
+    close(client);
+    close(server_fd);
+
+    return 0;
 }
