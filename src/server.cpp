add715f5bdf636098aebd74d59bd1b9e5a1fb170
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-07 22:59:38 +0530

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -1,103 +1,95 @@
-#include <alloca.h>
-#include <cstddef>
-#include <cstdio>
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <fstream>
+#ifdef __WIN32__
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <list>
-#include <thread>
-#include <regex>
-#include <sstream>
+#endif
 
-// Function to split a string by a delimiter
-std::list<char*> token_split_all(const char *token, void *string_to_split) {
-    std::list<char*> token_list;
-    char *pch;
-    pch = strtok((char*)string_to_split, token);
-    if (pch) token_list.push_back(pch);
-    while (pch != NULL) {
-        pch = strtok(NULL, token);
-        if (pch) token_list.push_back(pch);
-    }
-    return token_list;
-}
+using namespace std;
 
-// Function to extract the path from the HTTP request
-char* http_path(char *http_index) {
-    auto tokens = token_split_all(" ", http_index);
-    if (tokens.size() < 2) return nullptr;
-    return *(++tokens.begin());
-}
-
-int handle_http(int fd, struct sockaddr_in client_addr) {
-    char data[1024];
-    const char *data_sent_root_index = "HTTP/1.1 200 OK\r\n\r\n";
-    char *pch, *http_index, *uri;
-
-    std::cout << "Connection from: " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << "\n";
-    recv(fd, data, 1024, 0);
-    std::list<char*> lf_split_data = token_split_all("\r\n", data);
-    http_index = *lf_split_data.begin();
-    uri = http_path(http_index);
-    printf("http path: %s\n", uri);
-
-    if (strcmp(uri, "/") == 0) {
-        send(fd, data_sent_root_index, strlen(data_sent_root_index), 0);
-    } else if (strcmp(uri, "/user-agent") == 0) {
-        lf_split_data.pop_back();
-        for (auto rit = lf_split_data.rbegin(); rit != lf_split_data.rend(); ++rit) {
-            if (strncmp((const char*)*rit, "User-Agent:", 11) == 0) {
-                char sent_data[256];
-                pch = strchr(*rit, ' ');
-                if (!pch) {
-                    std::cout << "User Agent HeaderFormat Error!" << '\n';
-                    break;
-                }
-                pch++;
-                unsigned user_agent_len = strlen(pch);
-                strcpy(sent_data, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ");
-                strcat(sent_data, std::to_string(user_agent_len).c_str());
-                strcat(sent_data, "\r\n\r\n");
-                strcat(sent_data, pch);
-                send(fd, sent_data, strlen(sent_data), 0);
-                break;
-            }
-        }
-    } else {
-        pch = strtok(uri, "/");
-        const char *data_sent_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
-        if (pch && strcmp(pch, "echo") == 0) {
-            char sent_data[256];
-            pch = strtok(NULL, "/");
-            strcpy(sent_data, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ");
-            strcat(sent_data, std::to_string(strlen(pch)).c_str());
-            strcat(sent_data, "\r\n\r\n");
-            strcat(sent_data, pch);
-            send(fd, sent_data, strlen(sent_data), 0);
-        } else {
-            send(fd, data_sent_404, strlen(data_sent_404), 0);
+struct HttpRequest
+{
+    string method;
+    string path;
+    string version;
+    unordered_map<string, string> headers;
+    string body;
+};
+HttpRequest parseHttpRequest(const string request)
+{
+    HttpRequest httpRequest;
+    istringstream requestStream(request);
+    string line;
+    getline(requestStream, line);
+    istringstream lineStream(line);
+    lineStream >> httpRequest.method >> httpRequest.path >> httpRequest.version;
+    // Parse headers
+    while (getline(requestStream, line) && line != "\r")
+    {
+        auto colonPos = line.find(':');
+        if (colonPos != string::npos)
+        {
+            string headerName = line.substr(0, colonPos);
+            string headerValue = line.substr(colonPos + 2); // Skip the colon and space
+            httpRequest.headers[headerName] = headerValue;
         }
     }
-    close(fd);
-    return 0;
+    // Parse body (if any)
+    while (getline(requestStream, line))
+    {
+        httpRequest.body += line + "\n";
+    }
+    return httpRequest;
 }
+string extractEchoString(const string &path)
+{
+    const string echoPrefix = "/echo/";
+    auto startPos = path.find(echoPrefix);
+    if (startPos != string::npos)
+    {
+        return path.substr(startPos + echoPrefix.size());
+    }
+    return string(); // Return an empty string
+}
+int main(int argc, char **argv)
+{
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    cout << "Logs from your program will appear here!\n";
+    string dir;
+    if (argc == 3 && strcmp(argv[1], "--directory") == 0)
+    {
+        dir = argv[2];
+    }
 
-int main(int argc, char **argv) {
+    // Uncomment this block to pass the first stage
+    //
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        std::cerr << "Failed to create server socket\n";
+    if (server_fd < 0)
+    {
+        cerr << "Failed to create server socket\n";
         return 1;
     }
 
+    // Since the tester restarts your program quite often, setting REUSE_PORT
+    // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        std::cerr << "setsockopt failed\n";
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+    {
+        cerr << "setsockopt failed\n";
         return 1;
     }
 
@@ -106,31 +98,77 @@ int main(int argc, char **argv) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(4221);
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
-        std::cerr << "Failed to bind to port 4221\n";
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+    {
+        cerr << "Failed to bind to port 4221\n";
         return 1;
     }
 
     int connection_backlog = 5;
-    if (listen(server_fd, connection_backlog) != 0) {
-        std::cerr << "listen failed\n";
+    if (listen(server_fd, connection_backlog) != 0)
+    {
+        cerr << "listen failed\n";
         return 1;
     }
 
     struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+    int client_addr_len = sizeof(client_addr);
 
-    std::cout << "Waiting for a client to connect...\n";
-
-    while (true) {
-        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (client_fd < 0) {
-            std::cerr << "Error in accepting client\n";
-        } else {
-            std::cout << "Client connected\n";
-            std::thread th(handle_http, client_fd, client_addr);
-            th.detach();
+    cout << "Waiting for a client to connect...\n";
+    while (true)
+    {
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+        cout << "Client connected\n";
+        char buffer[1024] = {0};
+        ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received < 0)
+        {
+            cerr << "Failed to receive data\n";
+            close(client_fd);
+            close(server_fd);
+            return 1;
         }
+        buffer[bytes_received] = '\0';
+        HttpRequest request = parseHttpRequest(string(buffer));
+        string responsePath = extractEchoString(request.path);
+        string http_response;
+        if (request.path == "/user-agent")
+        {
+            string userAgent = request.headers["User-Agent"];
+            http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
+            http_response += "Content-Length: " + to_string(userAgent.length() - 1) + "\r\n\r\n" + userAgent;
+        }
+        else if (request.path == "/")
+        {
+            http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"; // Return 200 for root path
+        }
+        else if (request.path.substr(0, 6) == "/echo/")
+        {
+            http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
+            http_response += "Content-Length: " + to_string(responsePath.length()) + "\r\n\r\n" + responsePath;
+        }
+        else if (request.path.substr(0, 7) == "/files/")
+        {
+            string fileName = request.path.substr(7);
+            ifstream ifs(dir + fileName);
+            if (ifs.good())
+            {
+                stringstream content;
+                content << ifs.rdbuf();
+                stringstream respond("");
+                http_response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + to_string(content.str().length()) + "\r\n\r\n" + content.str() + "\r\n";
+            }
+            else
+            {
+                http_response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+        }
+        else
+        {
+            http_response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+
+        send(client_fd, http_response.c_str(), http_response.size(), 0);
     }
 
     close(server_fd);
