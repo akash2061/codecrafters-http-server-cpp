927b0bb388bd7ade0cd06e2f763eed3b7ebb3204
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-08 20:04:17 +0530

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -7,191 +7,159 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <vector>
-#include <sstream>
-#include <unordered_map>
 #include <thread>
 #include <fstream>
-#include <filesystem>
-namespace fs = std::filesystem;
-struct HttpRequest
+std::string directory_path = "/tmp/";
+void handle_client(int client_fd)
 {
-    std::string method;
-    std::string path;
-    std::string version;
-    std::string body;
-    std::unordered_map<std::string, std::string> headers;
-};
-struct HttpResponse
-{
-    std::string version;
-    std::string status;
-    std::string statusString;
-    std::unordered_map<std::string, std::string> headers;
-    std::string body;
-};
-std::string makeHttpResponse(HttpResponse response)
-{
-    std::ostringstream out;
-    const char *sep = " ";
-    const char *crlf = "\r\n";
-    out << response.version << sep << response.status << sep << response.statusString << crlf;
-    for (auto it = response.headers.begin(); it != response.headers.end(); it++)
+    char buffer[1024] = {0};
+    int read_bytes = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (read_bytes < 0)
     {
-        out << it->first << ": " << it->second << crlf;
-    }
-    out << crlf << response.body << crlf;
-    return out.str();
-}
-HttpRequest parseHttpRequest(const std::string request)
-{
-    HttpRequest httpRequest;
-    std::istringstream requestStream(request);
-    std::string line;
-    std::getline(requestStream, line);
-    std::istringstream lineStream(line);
-    lineStream >> httpRequest.method >> httpRequest.path >> httpRequest.version;
-    while (std::getline(requestStream, line) && line != "\r")
-    {
-        auto colonPos = line.find(':');
-        auto crPos = line.find_last_of('\r');
-        if (colonPos != std::string::npos)
-        {
-            std::string headerName = line.substr(0, colonPos);
-            std::string headerValue = line.substr(colonPos + 2);
-            auto crPos = headerValue.find_last_of('\r');
-            headerValue = headerValue.substr(0, crPos);
-            httpRequest.headers[headerName] = headerValue;
-        }
-    }
-    while (std::getline(requestStream, line))
-    {
-        httpRequest.body += line;
-    }
-    return httpRequest;
-}
-int readFile(const std::string filePath, std::string &body)
-{
-    std::ifstream infile(filePath);
-    infile.seekg(0, infile.end);
-    long fileSize = infile.tellg();
-    infile.seekg(0, infile.beg);
-    char *buffer = new char[fileSize];
-    infile.read(buffer, fileSize);
-    body = std::string(buffer);
-    delete buffer;
-    infile.close();
-    return fileSize;
-}
-void handle_client(const int socket_fd, const std::string &directory)
-{
-    if (socket_fd < 0)
-    {
-        std::cerr << "Socket not connected\n";
+        std::cerr << "Failed to read from client\n";
+        close(client_fd);
         return;
     }
-    std::cout << "Socket Connected\n";
-    char buffer[2048] = {0};
-    ssize_t bytes_received = 0;
-    bytes_received = read(socket_fd, buffer, sizeof(buffer));
-    if (bytes_received < 0)
+    std::string request(buffer);
+    std::string::size_type pos = request.find(' ');
+    if (pos != std::string::npos)
     {
-        std::cerr << "Failed to receive data\n";
-        close(socket_fd);
-        return;
-    }
-    buffer[bytes_received] = '\0';
-    HttpRequest request = parseHttpRequest(std::string(buffer));
-    HttpResponse response;
-    response.version = "HTTP/1.1";
-    if (request.path == "/")
-    {
-        response.status = "200";
-        response.statusString = "OK";
-    }
-    else if (request.path.rfind("/echo", 0) == 0)
-    {
-        std::string rbody = request.path.substr(6);
-        response.headers["Content-Type"] = "text/plain";
-        response.headers["Content-Length"] = std::to_string(rbody.size());
-        response.status = "200";
-        response.statusString = "OK";
-        response.body = rbody;
-    }
-    else if (request.path == "/user-agent")
-    {
-        std::string rbody = request.headers["User-Agent"];
-        response.headers["Content-Type"] = "text/plain";
-        response.headers["Content-Length"] = std::to_string(rbody.size());
-        response.status = "200";
-        response.statusString = "OK";
-        response.body = rbody;
-    }
-    else if (request.path.rfind("/files/", 0) == 0)
-    {
-        std::string rbody = request.path.substr(7);
-        std::string filePath = directory + rbody;
-        if (!fs::exists(fs::path(filePath)) && request.method == "GET")
+        std::string method = request.substr(0, pos);
+        std::string::size_type pos2 = request.find(' ', pos + 1);
+        if (pos2 != std::string::npos)
         {
-            std::cerr << "File does not exist\n";
-            response.status = "404";
-            response.statusString = "Not Found";
-        }
-        else if (request.method == "GET")
-        {
-            std::string respBody;
-            long fileSize = readFile(filePath, respBody);
-            std::stringstream ss;
-            ss << fileSize;
-            response.status = "200";
-            response.statusString = "OK";
-            response.headers["Content-Length"] = ss.str();
-            response.headers["Content-Type"] = "application/octet-stream";
-            response.body = respBody;
-        }
-        else if (request.method == "POST")
-        {
-            std::ofstream outfile(filePath);
-            outfile << request.body;
-            outfile.close();
-            response.status = "201";
-            response.statusString = "Created";
+            std::string path = request.substr(pos + 1, pos2 - pos - 1);
+            std::string accept_encoding;
+
+            std::string::size_type ae_pos = request.find("Accept-Encoding: ");
+            if (ae_pos != std::string::npos)
+            {
+                ae_pos += 17;
+                std::string::size_type ae_end = request.find("\r\n", ae_pos);
+                if (ae_end != std::string::npos)
+                {
+                    accept_encoding = request.substr(ae_pos, ae_end - ae_pos);
+                }
+            }
+            std::string http_response;
+            if (path == "/")
+            {
+                http_response = "HTTP/1.1 200 OK\r\n\r\n";
+            }
+            else if (path.find("/echo/") == 0)
+            {
+                std::string echo_string = path.substr(6);
+                std::string content_length = std::to_string(echo_string.size());
+                http_response = "HTTP/1.1 200 OK\r\n";
+                if (accept_encoding == "gzip")
+                {
+                    http_response += "Content-Encoding: gzip\r\n";
+                }
+                http_response += "Content-Type: text/plain\r\n";
+                http_response += "Content-Length: " + content_length + "\r\n\r\n";
+                http_response += echo_string;
+            }
+            else if (path == "/user-agent")
+            {
+                std::string user_agent;
+                std::string::size_type ua_pos = request.find("User-Agent: ");
+                if (ua_pos != std::string::npos)
+                {
+                    ua_pos += 12;
+                    std::string::size_type ua_end = request.find("\r\n", ua_pos);
+                    if (ua_end != std::string::npos)
+                    {
+                        user_agent = request.substr(ua_pos, ua_end - ua_pos);
+                    }
+                }
+                std::string content_length = std::to_string(user_agent.size());
+                http_response = "HTTP/1.1 200 OK\r\n";
+                http_response += "Content-Type: text/plain\r\n";
+                http_response += "Content-Length: " + content_length + "\r\n\r\n";
+                http_response += user_agent;
+            }
+            else if (path.find("/files/") == 0)
+            {
+                std::string filename = path.substr(7);
+                std::string file_path = directory_path + filename;
+                if (method == "GET")
+                {
+                    std::ifstream file(file_path, std::ios::binary | std::ios::ate);
+                    if (file.is_open())
+                    {
+                        std::streamsize file_size = file.tellg();
+                        file.seekg(0, std::ios::beg);
+                        std::string content_length = std::to_string(file_size);
+                        http_response = "HTTP/1.1 200 OK\r\n";
+                        http_response += "Content-Type: application/octet-stream\r\n";
+                        http_response += "Content-Length: " + content_length + "\r\n\r\n";
+                        http_response += std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+                    }
+                    else
+                    {
+                        http_response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    }
+                }
+                else if (method == "POST")
+                {
+                    std::string::size_type content_length_pos = request.find("Content-Length: ");
+                    int content_length = 0;
+                    if (content_length_pos != std::string::npos)
+                    {
+                        content_length_pos += 16;
+                        std::string::size_type content_length_end = request.find("\r\n", content_length_pos);
+                        if (content_length_end != std::string::npos)
+                        {
+                            content_length = std::stoi(request.substr(content_length_pos, content_length_end - content_length_pos));
+                        }
+                    }
+                    // Find the start of the body (after the double CRLF)
+                    std::string::size_type body_pos = request.find("\r\n\r\n");
+                    if (body_pos != std::string::npos)
+                    {
+                        body_pos += 4;
+                        std::string body = request.substr(body_pos, content_length);
+                        // Write the body to the file
+                        std::ofstream outfile(file_path, std::ios::binary);
+                        outfile.write(body.c_str(), body.size());
+                        outfile.close();
+                        http_response = "HTTP/1.1 201 Created\r\n\r\n";
+                    }
+                    else
+                    {
+                        http_response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+                    }
+                }
+                else
+                {
+                    http_response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+                }
+            }
+            else
+            {
+                http_response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+            write(client_fd, http_response.c_str(), http_response.size());
         }
     }
-    else
-    {
-        response.status = "404";
-        response.statusString = "Not Found";
-    }
-    std::string http_response = makeHttpResponse(response);
-    ssize_t bytes_sent = send(socket_fd, http_response.c_str(), http_response.size(), 0);
-    if (bytes_sent < 0)
-    {
-        std::cerr << "Failed to send data\n";
-        close(socket_fd);
-        return;
-    }
-    close(socket_fd);
-    return;
+    close(client_fd);
 }
 int main(int argc, char **argv)
 {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
-    std::cout << argv[2] << "\n";
-    const std::string directory = std::string(argv[2]);
-    std::cout << "[DIR] " << directory << "\n";
-    // Uncomment this block to pass the first stage
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+    if (argc >= 3 && std::string(argv[1]) == "--directory")
+    {
+        directory_path = argv[2];
+    }
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0)
     {
         std::cerr << "Failed to create server socket\n";
         return 1;
     }
-    // Since the tester restarts your program quite often, setting REUSE_PORT
-    // ensures that we don't run into 'Address already in use' errors
     int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
     {
         std::cerr << "setsockopt failed\n";
         return 1;
@@ -200,34 +168,31 @@ int main(int argc, char **argv)
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(4221);
-
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
     {
         std::cerr << "Failed to bind to port 4221\n";
         return 1;
     }
-
     int connection_backlog = 5;
     if (listen(server_fd, connection_backlog) != 0)
     {
         std::cerr << "listen failed\n";
         return 1;
     }
-
-    struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-
-    std::cout << "Waiting for a client to connect...\n";
     while (true)
     {
-        int socket_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-        if (socket_fd < 0)
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        std::cout << "Waiting for a client to connect...\n";
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_fd < 0)
         {
-            std::cerr << "failed to connect the client\n";
-            return 1;
+            std::cerr << "Failed to accept client connection\n";
+            continue;
         }
-        std::thread thread_obj(handle_client, socket_fd, directory);
-        thread_obj.detach();
+        std::cout << "Client connected\n";
+        std::thread client_thread(handle_client, client_fd);
+        client_thread.detach();
     }
     close(server_fd);
     return 0;
